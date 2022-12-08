f7fed55f36e8f08b259b2f86453a966d3c97cf03
Julian Wischnat <julian.wischnat@stud.h-da.de>
2022-12-08 19:38:03 +0100

    add example with user and music

--- REST/microservices/server.js
+++ REST/microservices/server.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const gateway = require('express-gateway');
+import path from "path";
+import gateway from "express-gateway";
+import "./user";
+import "./music";
 
-gateway()
-  .load(path.join(__dirname, 'config'))
-  .run();
+gateway().load(path.join(__dirname, "config")).run();
