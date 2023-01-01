c3f915fd03938a5e4284a06096026556497d7387
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-01-01 20:16:12 +0100

    add ref links for Supergraph

--- GraphQl/gateway/index.js
+++ GraphQl/gateway/index.js
@@ -2,9 +2,42 @@
 const { ApolloServer } = require('@apollo/server');
 const { ApolloGateway, IntrospectAndCompose } = require('@apollo/gateway');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+// https://www.apollographql.com/docs/apollo-server/getting-started/
+// https://www.apollographql.com/docs/apollo-server/using-federation/api/apollo-subgraph/
+// https://www.apollographql.com/docs/apollo-server/using-federation/apollo-gateway-setup
+// https://www.apollographql.com/docs/apollo-server/using-federation/api/apollo-gateway/
+// https://www.apollographql.com/docs/federation/federated-types/overview/#supergraph-schema
+// https://www.apollographql.com/docs/federation/federated-types/composition/
+// https://www.apollographql.com/docs/federation/quickstart/local-subgraphs
 
+// * 1. Supergraph schema & Subgraph schema & API schema
+// *https://www.apollographql.com/docs/federation/federated-types/overview/#supergraph-schema
+// * Important for point 2
+// *The supergraph schema is the output of schema composition
+// * TODO create supergraph schema (supergraph.graphql)
 
-//https://www.apollographql.com/docs/apollo-server/using-federation/apollo-gateway-setup
+// * 1.1 Schema composition!
+// * https://www.apollographql.com/docs/federation/federated-types/composition/
+// * rover supergraph compose --config ./supergraph-config.yaml
+// * ! 1.2 Manually with the Rover CLI
+// * https://www.apollographql.com/docs/federation/quickstart/setup/#install-the-rover-cli
+// *Part 1 - Project setup & Part 3 - Local composition
+
+// ! 2. Implementing a gateway with Apollo Server
+// ! API Reference
+// https://www.apollographql.com/docs/apollo-server/using-federation/api/apollo-gateway/
+// ! Implementing and Examples
+// https://www.apollographql.com/docs/apollo-server/using-federation/apollo-gateway-setup
+// * TODO Implement -> const supergraphSdl = readFileSync('./supergraph.graphql').toString();
+// * TODO const gateway = new ApolloGateway({
+// * TODO  supergraphSdl,
+// * TODO  });
+// Name
+// You can specify any string value for name, which is used primarily for query planner output,
+// error messages, and logging.
+
+// ! Implementing a subgraph with Apollo Server
+// https://www.apollographql.com/docs/apollo-server/using-federation/apollo-subgraph-setup
 const gateway = new ApolloGateway({
   supergraphSdl: new IntrospectAndCompose({
     subgraphs: [
