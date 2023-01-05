ac720c6be922d963281c9670a32ba3993e65f0c0
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-01-05 13:02:36 +0100

    change path for author service

--- REST/services/Author/index.js
+++ REST/services/Author/index.js
@@ -1,27 +1,19 @@
 let express = require("express");
-const { getClient } = require('./connect');
+const { getClient } = require("./connect");
 let app = express();
 
-app.get("/actors", async (req, res, next) => {
+app.get("/authors", async (req, res, next) => {
   const client = await getClient();
 
-  const entries = await client.query('SELECT * FROM authors;');
+  const entries = await client.query("SELECT * FROM authors;");
   console.log(`Database entries for authors: ${entries.rowCount} row(s)`);
-  console.log(Object.keys(entries.rows?.[0]).join('\t'));
-  console.log(`${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`);
+  console.log(Object.keys(entries.rows?.[0]).join("\t"));
+  console.log(
+    `${entries.rows.map((r) => Object.values(r).join("\t")).join("\n")}`
+  );
   await client.end();
-  // let array_actors = [
-  //   "Tom Cruise",
-  //   "Johnny Depp",
-  //   "Di Caprio",
-  //   "Russel Crowe",
-  //   "Tom Hanks",
-  // ];
   res.json(entries.rows);
 });
-
-
-
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
