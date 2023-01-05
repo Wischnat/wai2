bee61c96a1343927200c77b7bc1fa36d62f7ad8d
Julian Wischnat <julian.wischnat@stud.h-da.de>
2023-01-05 11:55:11 +0100

    change the pk in authors table to serial

--- REST/services/Author/init.sql
+++ REST/services/Author/init.sql
@@ -2,11 +2,12 @@
 drop table if exists authors;
 
 CREATE TABLE authors (
-  author_id INT PRIMARY KEY,
+  author_id SERIAL PRIMARY KEY,
   firstname VARCHAR(255),
   lastname VARCHAR(255),
   age INT
 );
 
 
-INSERT INTO authors (author_id, firstname, lastname, age) VALUES (1, 'John', 'Doe', 25);
+INSERT INTO authors (firstname, lastname, age) VALUES ('John', 'Doe', 100);
+INSERT INTO authors (firstname, lastname, age) VALUES ('Alex', 'Wick', 55);
